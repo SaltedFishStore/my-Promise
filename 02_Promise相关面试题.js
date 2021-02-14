2b1ef8a3c66f401401dc79041d710a6b7cd05551
SaltedFishStore <liu17502002026@qq.com>
2021-02-14 10:51:34 +0800

    自定义Promise

---
+++ 02_Promise相关面试题.js
@@ -0,0 +1,14 @@
+setTimeout(() => {
+  console.log(1)
+}, 0)
+new Promise((resolve) => {
+  console.log(2)
+  resolve()
+}).then(() => {
+  console.log(3)
+}).then(() => {
+  console.log(4)
+})
+console.log(5)
+
+// 2, 5, 3, 5, 1
